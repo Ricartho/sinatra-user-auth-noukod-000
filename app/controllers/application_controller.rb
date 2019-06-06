45d002b915b287be1f622b6fadfcda9f4c064679
Ricartho Franck <ricartho.franck@esih.edu>
2019-06-06 15:13:18 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,6 +17,7 @@ class ApplicationController < Sinatra::Base
 
   post '/registrations' do
     binding.pry
+    @user = User.find_by(email: params["email"], password: params["password"])
     # @user.save
     # session[:user_id] = @user.id
     redirect '/users/home'
